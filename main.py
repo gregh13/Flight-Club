629e60fb34d23f8f8198700d92e7304824ddf6d2
Greg <gregh13@protonmail.com>
2022-06-10 18:56:32 +0700

    change from SQLite to Postgres Database

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ MAIN_URL = "http://127.0.0.1:5000/"
 Bootstrap(app)
 
 # LATER, during Heroku stage
-# uri = os.getenv("DATABASE_URL")
-# if uri and uri.startswith("postgres://"):
-#     uri = uri.replace("postgres://", "postgresql://", 1)
-# app.config['SQLALCHEMY_DATABASE_URI'] = uri
+uri = os.getenv("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///club-users.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///club-users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
